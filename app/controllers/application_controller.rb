9515ebb48eb6db41bdb53bc91b8e2ca1bb6dac63
elijahsamuels <elijahsamuels@gmail.com>
2021-01-16 23:05:19 -0700

    udpated meeting selection with input type date and time

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -95,16 +95,7 @@ class ApplicationController < Sinatra::Base
     #   @meeting.meeting_date
     #   binding.pry
 
-    # end
-
-    def datepicker_input form, field
-      content_tag :td, :data => {:provide => 'datepicker', 'date-format' => 'yyyy-mm-dd', 'date-autoclose' => 'true'} do
-        form.text_field field, class: 'form-control', placeholder: 'YYYY-MM-DD'
-      end
-    end
-
-
-  
+    # endgit   
     
   end
 
